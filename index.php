ab12f92bf5ae6631acd854661a8db99b9a95a744
Quentin CHAPEL <chapelquen@gmail.com>
2018-12-12 11:25:21 +0100

    [Feat][Test]Formatting json
    Rearranging data from JSON input

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
     header('Content-Type: application/json');
     $rawJSONPage = "https://pokeapi.co/api/v2/";
     $json = file_get_contents($rawJSONPage);
-    $json = json_decode(json_encode($json), true);
+    $json = json_decode($json);
     echo json_encode($json);
 ?>
