75328702321941dc6bb06df5357251520a887b70
Quentin CHAPEL <chapelquen@gmail.com>
2018-12-27 09:00:07 +0100

    [Fix]Fixing issues about routing
    Testing default DefaultController class from symfony website.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,10 +1,24 @@
 <?php
-    namespace App\Controller\DefaultController;
-    use Symfony\Bundle\FrameWorkBundle\Controller\Controller;
+namespace App\Controller;
 
-    class DefaultController extends Controller{
-        public function indexAction(){
-            return $this->render("index.php");
-        }
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        $posts = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findLatest();
+
+        return $this->render('default/index.html.twig', [
+            'posts' => $posts,
+        ]);
     }
+}
 ?>
