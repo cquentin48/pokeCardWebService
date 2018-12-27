194734275fee249906989defcf15877e48043e83
Quentin CHAPEL <chapelquen@gmail.com>
2018-12-27 08:25:41 +0100

    [Feat][Fix][Test]Fixing issues about json econding test page
    Fixing encoding issues about json_encode function use in PHP.

--- public/encodingJSON.php
+++ public/encodingJSON.php
@@ -1,7 +1,8 @@
 <?php
     header('Content-Type: application/json');
     $rawJSONPage = "https://pokeapi.co/api/v2/";
-    $json = file_get_contents($rawJSONPage);
-    $json = json_decode(json_encode($json), true);
-    echo json_encode($json);
+	$jsonRawData = file_get_contents($rawJSONPage);
+	$json = json_decode($jsonRawData, true);
+	$jsonEncoded = json_encode($json);
+	echo $jsonEncoded;
 ?>
