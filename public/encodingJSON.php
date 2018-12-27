112ee05bc3dc59e7830f0aad1be6f15e2944f226
Quentin CHAPEL <chapelquen@gmail.com>
2018-12-27 08:05:05 +0100

    [Feat][Test]Testing encoding JSONArray
    Testing if the heroku layer can display jsonarrays online.

--- public/encodingJSON.php
+++ public/encodingJSON.php
@@ -1,3 +1,7 @@
 <?php
-	echo "Hello World!";
+    header('Content-Type: application/json');
+    $rawJSONPage = "https://pokeapi.co/api/v2/";
+    $json = file_get_contents($rawJSONPage);
+    $json = json_decode(json_encode($json), true);
+    echo json_encode($json);
 ?>
