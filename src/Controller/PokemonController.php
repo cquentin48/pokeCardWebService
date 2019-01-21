1dc1c8b2683848de7caece68617b793206ffcda5
Quentin CHAPEL <chapelquen@gmail.com>
2019-01-21 17:45:51 +0100

    [Feat]Testing on Heroku if the pokemon can be read.
    Testing on Heroku.

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -21,16 +21,39 @@ class PokemonController extends AbstractController
         $jsonOutput['types'] = $json['types'];
         return $jsonOutput;
     }
+
+    function getAllPokemonBasicData()
+    {
+        $response = file_get_contents('https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?offset=0&limit=100');
+        $pokemons = array();
+        $pokemonSprites = array();
+        $returnedData = array();
+        $json = json_decode($response, true);
+        $results = $json['results'];
+        foreach($results as $result){
+            $returnedPokemonData = array();
+            $response = file_get_contents($result['url']);
+            $returnedJSONData = json_decode($response,true);
+            $returnedPokemonData['name'] = $result['name'];
+            $returnedPokemonData['sprite'] = $returnedJSONData['sprites']['back_default'];
+            array_push($returnedData, $returnedPokemonData);
+        }
+        return $returnedData;
+    }
     
     public function renderPokemonBasicInformations($id)
     {
         if($id == 0){
-            
+            return $this->render('index.html.php', array(
+                'jsonArray' => getAllPokemonBasicData()
+            ));
+        }else{
+            $jsonData = $this->loadJSONData("https://pokeapi.co/api/v2/pokemon/",$id);
+            return $this->render('index.html.php', array(
+                'jsonArray' => $jsonData
+            ));
         }
-        $jsonData = $this->loadJSONData("https://pokeapi.co/api/v2/pokemon/",$id);
-        return $this->render('index.html.php', array(
-            'jsonArray' => $jsonData
-        ));
+
     }
 }
 ?>
