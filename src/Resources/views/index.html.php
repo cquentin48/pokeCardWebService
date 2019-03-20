568b55c6687f73e4b1b3dfb8dd5018fd01c8c7f6
Quentin <cquentin48@gmail.com>
2019-03-20 11:22:07 +0100

    [Fix]Fixing php autoload import in index.html.php

--- src/Resources/views/index.html.php
+++ src/Resources/views/index.html.php
@@ -1,5 +1,5 @@
 <?php
-    require_once dirname(dirname(dirname(__DIR__))).'\vendor\autoload.php';
+    require_once dirname(dirname(dirname(__DIR__))).'/vendor/autoload.php';
     $jsonEncoded = json_encode($jsonArray);
 	echo $jsonEncoded;
 ?>
